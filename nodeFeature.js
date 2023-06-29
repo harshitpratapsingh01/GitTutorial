59aac7a4b67aa3af01e6dc06fe9247ece5c0ba4c
harshitpratapsingh01 <harshit.singh@appinventiv.com>
2023-06-29 23:11:02 +0530

    changes

---
+++ nodeFeature.js
@@ -0,0 +1,15 @@
+// var name = "abcd";
+
+// for(var i = 0; i<100000; i++){
+//     setTimeout(() => {
+//         console.log(name + i);
+//     },1000*i);
+// }
+
+console.log("hii");
+
+setTimeout(function(){
+    console.log("Harshit");
+},1000);
+
+console.log("My Name is");
