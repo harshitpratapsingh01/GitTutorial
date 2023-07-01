4534830b382f49497e81e74b7dca64e6cbe1c81d
harshitpratapsingh01 <harshit.singh@appinventiv.com>
2023-07-02 01:46:51 +0530

    comment

--- object.js
+++ object.js
@@ -6,5 +6,8 @@ let user = {
     }
 }
 
+/* Here Function 'func' is a property of object 'user' 
+so, function "func" is a method of object "user" */
+
 console.log(user.func());
 console.log(user.age);
